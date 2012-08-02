5ae99b9749cf2adde11caaca932634fff2aa8e83
Brie <bbunge7@gmail.com>
2012-08-01 20:57:48 -0700

    rotate nn

--- rotate-90-NbyN.js
+++ rotate-90-NbyN.js
@@ -11,6 +11,14 @@ var m = [
   [13,14,15,16]
 ];
 
+var m = [
+  [1,2,3,4,5],
+  [6,7,8,9,10],
+  [11,12,13,14,15],
+  [16,17,18,19,20],
+  [21,22,23,24,25]
+];
+
 function printMatrix(m) {
   for (var i=0; i < m.length; i++) {
     var row = '';
@@ -22,46 +30,39 @@ function printMatrix(m) {
 }
 
 function rotateMatrix(m) {
-  var maxY = m.length - 1;
-  var maxX = Math.floor(m.length / 2);
-  var x, tmp, tmp2;
   var minY = 0;
-  for (var startx = 0; startx < maxX; startx++) {
-    console.log(startx);
-    console.log(minY);
-    var n = maxY - minY;
+  var maxY = m.length - 1;
+  var minX = 0;
+  var maxX = Math.floor(m.length / 2);
+  var x, n, tmp, tmp2;
+  for (; minX < maxX; minX++) {
+    n = maxY - minY;
     for (var i = 0; i < n; i++) {
       y = minY + i
-      x = startx;
-
-      console.log("i: " + i);
-      console.log("x: " + x + " y: " + y);
+      x = minX;
       tmp = m[x][y];
+      m[x][y] = m[x+(n-i)][y-i];
+      m[x+(n-i)][y-i] = m[]
 
       x += i;
       y += n - i;
-      console.log("x: " + x + " y: " + y);
       tmp2 = m[x][y];
       m[x][y] = tmp;
 
       x += n - i;
       y -= i;
       tmp = m[x][y];
-      console.log("x: " + x + " y: " + y);
       m[x][y] = tmp2;
 
       x -= i;
       y -= n - i;
-      console.log("x: " + x + " y: " + y);
       tmp2 = m[x][y];
       m[x][y] = tmp;
 
       x -= n - i;
       y += i;
-      console.log("x: " + x + " y: " + y);
       tmp = m[x][y];
       m[x][y] = tmp2;
-
     };
     minY++;
     maxY--;
