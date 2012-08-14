f24434affe3fb211148f9ec80d2686d8dc3a8489
Brie <bbunge7@gmail.com>
2012-08-13 22:30:08 -0700

    desc sorting too

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,8 +1,8 @@
-function sort(array) {
+function sort(array, isAsc) {
   for (var i = 1; i < array.length; i++) {
     var tmp = array[i];
     var j = i;
-    while (j > 0 && tmp < array[j-1]) {
+    while (j > 0 && isAsc ? tmp < array[j-1] : tmp > array[j-1]) {
       array[j] = array[j-1];
       j--;
     }
@@ -11,5 +11,8 @@ function sort(array) {
 }
 
 var array = [3,5,1,7,2];
-sort(array);
+sort(array, true);
 console.log(array);
+sort(array, false);
+console.log(array);
+
