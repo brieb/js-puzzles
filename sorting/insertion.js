7b302fc9bdb2318460975bbbdd80a8361611b8e5
Brie <bbunge7@gmail.com>
2012-08-13 22:20:38 -0700

    insertion v2

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,11 +1,11 @@
 function sort(array) {
   for (var i = 1; i < array.length; i++) {
-    var j = i;
-    while (j > 0 && array[i] < array[j-1])
-      j--;
     var tmp = array[i];
-    for (var k = i; k > j; k--)
-      array[k] = array[k-1];
+    var j = i;
+    while (j > 0 && tmp < array[j-1]) {
+      array[j] = array[j-1];
+      j--;
+    }
     array[j] = tmp;
   }
 }
